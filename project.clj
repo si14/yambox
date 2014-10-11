ad759d048627ff92c634385b587352ed22addc8f
alwxndr <alwxndr@gmail.com>
2014-10-11 14:08:56 +0400

    initial project

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject yambox "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://yambox.org"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
