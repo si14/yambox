b53fbba61458be5bdc83aaa60ad3861b11927814
Dmitry Groshev <lambdadmitry@gmail.com>
2014-10-11 17:36:33 +0400

    make OAuth2 work

---
+++ src/clj/friend_oauth2/workflow.clj
@@ -0,0 +1,58 @@
+(ns friend-oauth2.workflow
+  (:require
+   [friend-oauth2.util :as util]
+   [cemerick.friend :as friend]
+   [clj-http.client :as client]
+   [ring.util.request :as request]))
+
+(defn- default-credential-fn
+  [creds]
+  {:identity (:access-token creds)})
+
+(defn- is-oauth2-callback?
+  [config request]
+  (or (= (request/path-info request)
+         (get-in config [:client-config :callback :path]))
+      (= (request/path-info request)
+         (or (:login-uri config) (-> request ::friend/auth-config :login-uri)))))
+
+(defn- request-token
+  "POSTs request to OAauth2 provider for authorization token."
+  [{:keys [uri-config access-token-parsefn]} code]
+  (let [access-token-uri (:access-token-uri uri-config)
+        query-map        (merge {:grant_type "authorization_code"}
+                                (util/replace-authz-code access-token-uri code))
+        token-url        (assoc access-token-uri :query query-map)
+        token-response   (client/post (:url token-url) {:form-params (:query token-url)})
+        token-parse-fn   (or access-token-parsefn util/extract-access-token)]
+    (token-parse-fn token-response)))
+
+(defn- redirect-to-provider!
+  "Redirects user to OAuth2 provider. Code should be in response."
+  [{:keys [uri-config]} request]
+  (let [anti-forgery-token    (util/generate-anti-forgery-token)
+        session-with-af-token (assoc (:session request) (keyword anti-forgery-token) "state")]
+    (-> uri-config
+        (util/format-authn-uri anti-forgery-token)
+        ring.util.response/redirect
+        (assoc :session session-with-af-token))))
+
+(defn workflow
+  "Workflow for OAuth2"
+  [config]
+  (fn [request]
+    (if (is-oauth2-callback? config request)
+      ;; Extracts code from request if we are getting here via OAuth2 callback.
+      ;; http://tools.ietf.org/html/draft-ietf-oauth-v2-31#section-4.1.2
+      (let [{:keys [state code]} (:params request)
+            session-state        (util/extract-anti-forgery-token request)]
+        (prn "state" state "session-state" session-state)
+        (if (and (not (nil? code))
+                 (or true (= state session-state)))
+          (when-let [access-token (request-token config code)]
+            (when-let [auth-map ((:credential-fn config default-credential-fn)
+                                 {:access-token access-token})]
+              (vary-meta auth-map merge {::friend/workflow :oauth2
+                                         ::friend/redirect-on-auth? true
+                                         :type ::friend/auth})))
+          (redirect-to-provider! config request))))))
--- src/clj/yambox/core.clj
+++ src/clj/yambox/core.clj
@@ -1,6 +1,7 @@
 (ns yambox.core
   (:require
    [clojure.java.io :as io]
+   [clojure.string :as s]
    [plumbing.core :as p]
    [cemerick.friend :as friend]
    [cemerick.friend
@@ -15,6 +16,8 @@
    [ring.util.response :as resp]
    [ring.middleware.anti-forgery :as raf]
    [ring.middleware.defaults :as rmd]
+   [friend-oauth2.workflow :as oauth2]
+   [friend-oauth2.util :refer [format-config-uri]]
    [yambox.templates :as tpl]
    [nomad :as nom :refer [defconfig]])
   (:gen-class))
@@ -22,24 +25,91 @@
 (defconfig main-config
   (io/resource "main-config.edn"))
 
+(defn credential-fn
+  [token]
+  {:identity token
+   :roles #{::user}})
+
+(defn get-uri-config []
+  (let [client-config (p/safe-get (main-config) :oauth)
+        required-rights-str (->> :required-rights
+                                 (p/safe-get client-config)
+                                 (map name)
+                                 (s/join " "))]
+    {:authentication-uri {:url "https://sp-money.yandex.ru/oauth/authorize"
+                          :query {:client_id (:client-id client-config)
+                                  :response_type "code"
+                                  :redirect_uri (format-config-uri client-config)
+                                  :scope required-rights-str}}
+
+     :access-token-uri {:url "https://sp-money.yandex.ru/oauth/token"
+                        :query {:client_id (:client-id client-config)
+                                :client_secret (:client-secret client-config)
+                                :grant_type "authorization_code"
+                                :redirect_uri (format-config-uri client-config)}}}))
+
+(def friend-config
+  {:allow-anon? true
+   :workflows [(oauth2/workflow
+                {:client-config (p/safe-get (main-config) :oauth)
+                 :uri-config (get-uri-config)
+                 :credential-fn credential-fn})]})
+
+(c/defroutes secure-routes
+  (c/GET "/page" req
+    (let [token (-> req
+                    :session
+                    :cemerick.friend/identity
+                    :current
+                    :access-token)
+          resp (http/post "https://money.yandex.ru/api/account-info"
+                      {:accept :json
+                       :oauth-token token
+                       :as :json})]
+      (->> resp
+           :body
+           :balance
+           str))))
+
 (defn static-html
   [file-name]
   (-> file-name
-    (resp/resource-response {:root "public"})
-    (assoc :headers {"Content-Type" "text/html"})))
+      (resp/resource-response {:root "public"})
+      (assoc :headers {"Content-Type" "text/html"})))
 
 (c/defroutes routes
   (c/GET "/" [] (static-html "index.html"))
   (c/GET "/create" [] (tpl/page-create))
-  (route/resources "/")
-  (route/not-found "Page not found"))
+  (route/resources "/"))
 
 (def handler
   (->
-    (c/routes routes)
-    (rmd/wrap-defaults (-> rmd/site-defaults
-                         (assoc-in [:security :anti-forgery] false)))))
+   (c/routes
+    routes
+    (c/context "/secure" req
+               (friend/wrap-authorize secure-routes #{::user}))
+    (route/not-found "Page not found"))
+   (friend/authenticate friend-config)
+   (rmd/wrap-defaults (-> rmd/site-defaults
+                          (assoc-in [:security :anti-forgery] false)))))
+
+(defonce server (atom nil))
+
+(defn start []
+  (let [port (p/safe-get (main-config) :port)
+        new-server (run-jetty handler {:port port
+                                       :join? false})]
+    (reset! server new-server)))
+
+(defn stop []
+  (.stop @server))
+
+(defn restart []
+  (when @server
+    (stop))
+  (start))
 
 (defn -main
   [& args]
-  (run-jetty handler (p/safe-get (main-config) :port)))
+  (let [port (p/safe-get (main-config) :port)]
+    (run-jetty handler {:port port})))
