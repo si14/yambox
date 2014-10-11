96659b2ec805f842e9b050049d03f24b7b6256e4
Dmitry Groshev <lambdadmitry@gmail.com>
2014-10-11 16:39:06 +0400

    fix setup/deploy

--- infrastructure/gzip.sh
+++ infrastructure/gzip.sh
@@ -3,3 +3,4 @@
 for i in /opt/yambox/resources/public/css/*; do [ "${i:(-3)}" == ".gz" ] || gzip -c -9 "$i" > "$i.gz"; done
 for i in /opt/yambox/resources/public/js/*; do [ "${i:(-3)}" == ".gz" ] || gzip -c -9 "$i" > "$i.gz"; done
 for i in /opt/yambox/resources/public/static_js/*; do [ "${i:(-3)}" == ".gz" ] || gzip -c -9 "$i" > "$i.gz"; done
+for i in /opt/yambox/resources/public/*; do [ "${i:(-3)}" == ".gz" ] || gzip -c -9 "$i" > "$i.gz"; done
