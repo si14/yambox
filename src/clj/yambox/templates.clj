264db2332512594cc0b6b41573422288f758cda2
alwxndr <alwxndr@gmail.com>
2014-10-12 01:41:32 +0400

    landing page features list

--- src/clj/yambox/templates.clj
+++ src/clj/yambox/templates.clj
@@ -27,7 +27,7 @@
    "&copy 2014 "
    [:b "Sugar Glider"]
    " team for "
-   [:a {:href "https://tech.yandex.ru/events/meetings/october-2014/"
+   [:a {:href   "https://tech.yandex.ru/events/meetings/october-2014/"
         :target "_blank"}
     "Yandex.Money Hackathon"]])
 
@@ -58,14 +58,12 @@
              (wrap-top [:a {:href "/login"} "Войти в аккаунт"])
              [:div.landing
               [:div.info
-               [:h1 "Собирай деньги проще и эффективнее!"]
-               "YamBox — удивительный инструмент для коллективного сбора денег. "
-               [:br]
-               "Собирай так, чтобы все были довольны."
+               [:h1 "Если не мой кошелёк, то чей?"]
+               "Собираете деньги на добрые дела? Добро должно быть открытым."
                [:p
                 [:a.btn.btn-default
                  {:href "/management/create-campaign"}
-                 "Начать кампанию сейчас!"]]]
+                 "Я тоже так считаю"]]]
               [:div.copyright
                "Image by "
                [:a {:href   "https://www.flickr.com/photos/hktang/5369651068/",
@@ -73,50 +71,62 @@
                 "Xiaojun Deng"]
                ". Licenced under Creative Commons 2.0."]]
              (wrap-yandex-social)
+             [:div.features
+              [:div.container
+               [:div.col-sm-3]
+               [:div.col-sm-6
+                [:h2 "YamBox это:"]
+                [:ol
+                 [:li "удобный виджет, показывающий количество собранных денег;"]
+                 [:li "открытая статистика, анонимизирующая жертвователей;"]
+                 [:li "способ безопасно передать код протекции собирающему;"]
+                 [:li "«голосование деньгами»."]]
+                ]
+               [:div.col-sm-3]]]
              (wrap-footer)]}))
 
 (defn page-management
   [req]
   (make-html
     {:title "Управление кампанией — YamBox"
-     :body [:div
-            (wrap-top [:a {:href "/logout"} "Выйти"])
-            [:div.separator]
-            [:div.container
-             [:div.col-sm-6
-              [:h1 "Моя кампания"]]
-             [:div.col-sm-6
-              "Test"]]]}))
+     :body  [:div
+             (wrap-top [:a {:href "/logout"} "Выйти"])
+             [:div.separator]
+             [:div.container
+              [:div.col-sm-6
+               [:h1 "Моя кампания"]]
+              [:div.col-sm-6
+               "Test"]]]}))
 
 (defn page-management-create
   [req]
   (make-html
     {:title "Создание кампании — YamBox"
-     :body [:div
-            (wrap-top [:a {:href "/logout"} "Выйти"])
-            [:div.separator]
-            [:div.container.page
-             [:div.col-sm-8.form
-              [:form {:role "form"}
-               [:div.form-group
-                [:input.header.control {:placeholder "Введите название"}]
-                [:div.descr "Например, "
-                 [:a "Сбор денег для проведения избирательной кампании"]]]
-               [:div.form-group
-                [:label "Ссылка на страницу кампании:"]
-                [:input.control]]
-               [:div.form-group
-                [:label "Сумма для сбора (в рублях):"]
-                [:input.control {:type "number"}]]
-               [:input.btn.btn-default {:type "submit" :value "Создать"}]]]
-             [:div.col-sm-4.tips
-              "Обратите внимание, что вы можете вести только одну кампанию одновременно.
-              После создания кампании вы можете поменять название и сумму для сбора,
-              однако изменение ссылки будет недоступно."]]
-            (wrap-footer)]}))
+     :body  [:div
+             (wrap-top [:a {:href "/logout"} "Выйти"])
+             [:div.separator]
+             [:div.container.page
+              [:div.col-sm-8.form
+               [:form {:role "form"}
+                [:div.form-group
+                 [:input.header.control {:placeholder "Введите название"}]
+                 [:div.descr "Например, "
+                  [:a "Сбор денег для проведения избирательной кампании"]]]
+                [:div.form-group
+                 [:label "Ссылка на страницу кампании:"]
+                 [:input.control]]
+                [:div.form-group
+                 [:label "Сумма для сбора (в рублях):"]
+                 [:input.control {:type "number"}]]
+                [:input.btn.btn-default {:type "submit" :value "Создать"}]]]
+              [:div.col-sm-4.tips
+               "Обратите внимание, что вы можете вести только одну кампанию одновременно.
+               После создания кампании вы можете поменять название и сумму для сбора,
+               однако изменение ссылки будет недоступно."]]
+             (wrap-footer)]}))
 
 (defn page-campaign
   [req]
   (make-html
     {:title ""
-     :body ""}))
+     :body  ""}))
